0bc57b67330e645d8ace68d1e53f9488b64656a2
Vikash Kumar <vikashkrjsr15@gmail.com>
2025-08-08 13:01:44 +0530

    Time: 1 ms (86.65%), Space: 58.8 MB (27.62%) - LeetHub

---
+++ 3194-find-words-containing-character/3194-find-words-containing-character.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+    return words
+        .map((word, index) => word.includes(x) ? index : -1)
+        .filter(index => index !== -1);
+};
